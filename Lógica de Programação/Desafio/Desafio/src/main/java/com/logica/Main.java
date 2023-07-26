124ee106813ab460079c038baa082d0aed771e33
DanielleJeanine <danielle.jeanine92@gmail.com>
2023-07-26 00:56:19 -0300

    Desafio Lista de Alunos

---
+++ Lógica de Programação/Desafio/Desafio/src/main/java/com/logica/Main.java
@@ -0,0 +1,12 @@
+package com.logica;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Programa Iniciado!");
+        //Teste teste = new Teste();
+        //teste.menu();
+
+        ListaDeAlunos alunos = new ListaDeAlunos();
+        alunos.menu();
+    }
+}
